0489ba7a60678417fc680127bb09bfd3a4ac4764
dukky <andy.duks@gmail.com>
2013-08-07 23:57:31 +0100

    Improved examples, added ant build script and jar.

--- base62/src/im/duk/base62/Base62.java
+++ base62/src/im/duk/base62/Base62.java
@@ -50,10 +50,13 @@ public class Base62 {
 	 * Encodes a decimal value to a Base62 <code>String</code>.
 	 * 
 	 * @param b10
-	 *            the decimal value to encode.
+	 *            the decimal value to encode, must be nonnegative.
 	 * @return the number encoded as a Base62 <code>String</code>.
 	 */
 	public String encodeBase10(long b10) {
+		if (b10 < 0) {
+			throw new IllegalArgumentException("b10 must be nonnegative");
+		}
 		String ret = "";
 		while (b10 > 0) {
 			ret = characters.charAt((int) (b10 % 62)) + ret;
@@ -91,28 +94,36 @@ public class Base62 {
 	}
 
 	// Examples
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+
 		// Create a Base62 object using the default charset.
 		Base62 base62 = new Base62();
-		
+
 		// Create a Base62 object with an alternate charset.
-		Base62 base62alt = new Base62("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
-		
-		// Convert 1673 to Base62 (qZ). 
-		System.out.println(base62.encodeBase10(1673));
-		
+		Base62 base62alt = new Base62(
+				"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+
+		// Convert 1673 to Base62 (qZ).
+		System.out.println("1673 encoded to Base62: "
+				+ base62.encodeBase10(1673));
+
 		// Convert 1673 to Base62 with the alternate character set (A9).
-		System.out.println(base62alt.encodeBase10(1673));
-		
+		System.out.println("1673 encoded with alternate charset: "
+				+ base62alt.encodeBase10(1673));
+
 		// Convert nHkl3S4B to decimal (83,458,179,955,437).
-		System.out.println(base62.decodeBase62("nHkl3S4B"));
-		
+		System.out.println("nHkl3S4B decoded from Base62: "
+				+ base62.decodeBase62("nHkl3S4B"));
+
 		// Encoding and decoding a number returns the original result.
-		System.out.println(base62.decodeBase62(base62.encodeBase10(79872394879238L)));
-		
+		System.out.println("32442342 encoded to Base62 and back again: "
+				+ base62.decodeBase62(base62.encodeBase10(32442342)));
+
 		// Using invalid characters throws a runtime exception.
+		// Output was out of order with ant.
+		Thread.sleep(100);
 		try {
+			// Doesn't work
 			System.out.println(base62.decodeBase62("_j+j%"));
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
