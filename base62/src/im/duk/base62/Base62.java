7613cebe37685f186f35a3f0ba868dac9fbda983
dukky <andy.duks@gmail.com>
2013-08-10 23:32:36 +0100

    Undone deletions and added better comments.

--- base62/src/im/duk/base62/Base62.java
+++ base62/src/im/duk/base62/Base62.java
@@ -93,7 +93,36 @@ public class Base62 {
 
 	// Examples
 	public static void main(String[] args) throws InterruptedException {
+		// Create a Base62 object using the default charset.
+		Base62 base62 = new Base62();
+		System.gc();
+		// Create a Base62 object with an alternate charset.
+		Base62 base62alt = new Base62("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
 
+		// Convert 1673 to Base62 (qZ).
+		System.out.println("1673 encoded to Base62: " + base62.encodeBase10(1673));
+
+		// Convert 1673 to Base62 with the alternate character set (A9).
+		System.out.println("1673 encoded with alternate charset: " + base62alt.encodeBase10(1673));
+
+		// Convert nHkl3S4B to decimal (83,458,179,955,437).
+		System.out.println("nHkl3S4B decoded from Base62: " + base62.decodeBase62("nHkl3S4B"));
+
+		// Encoding and decoding a number returns the original result.
+		System.out.println("32442342 encoded to Base62 and back again: "
+				+ base62.decodeBase62(base62.encodeBase10(32442342)));
+
+		// Using invalid characters throws a runtime exception.
+		// Output was out of order with ant, adding this short sleep fixes
+		// things:
+		// The problem seems to be with the way ant's output handles system.err
+		Thread.sleep(100);
+		try {
+			// Doesn't work
+			System.out.println(base62.decodeBase62("_j+j%"));
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
