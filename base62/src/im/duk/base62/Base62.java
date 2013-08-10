3919c2bdf9207774b5655ff1cd29f27a4895bead
dukky <andy.duks@gmail.com>
2013-08-10 23:29:40 +0100

    Messing stuff up on the test branch

--- base62/src/im/duk/base62/Base62.java
+++ base62/src/im/duk/base62/Base62.java
@@ -40,8 +40,7 @@ public class Base62 {
 	 */
 	public Base62(String characters) {
 		if (!(characters.length() == 62)) {
-			throw new IllegalArgumentException(
-					"Invalid string length, must be 62.");
+			throw new IllegalArgumentException("Invalid string length, must be 62.");
 		}
 		this.characters = characters;
 	}
@@ -79,8 +78,7 @@ public class Base62 {
 	public long decodeBase62(String b62) {
 		for (char character : b62.toCharArray()) {
 			if (!characters.contains(String.valueOf(character))) {
-				throw new IllegalArgumentException(
-						"Invalid character(s) in string: " + character);
+				throw new IllegalArgumentException("Invalid character(s) in string: " + character);
 			}
 		}
 		long ret = 0;
@@ -95,40 +93,7 @@ public class Base62 {
 
 	// Examples
 	public static void main(String[] args) throws InterruptedException {
-		// Create a Base62 object using the default charset.
-		Base62 base62 = new Base62();
-		System.gc();
-		// Create a Base62 object with an alternate charset.
-		Base62 base62alt = new Base62(
-				"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
 
-		// Convert 1673 to Base62 (qZ).
-		System.out.println("1673 encoded to Base62: "
-				+ base62.encodeBase10(1673));
 
-		// Convert 1673 to Base62 with the alternate character set (A9).
-		System.out.println("1673 encoded with alternate charset: "
-				+ base62alt.encodeBase10(1673));
-
-		// Convert nHkl3S4B to decimal (83,458,179,955,437).
-		System.out.println("nHkl3S4B decoded from Base62: "
-				+ base62.decodeBase62("nHkl3S4B"));
-
-		// Encoding and decoding a number returns the original result.
-		System.out.println("32442342 encoded to Base62 and back again: "
-				+ base62.decodeBase62(base62.encodeBase10(32442342)));
-
-		// Using invalid characters throws a runtime exception.
-		// Output was out of order with ant.
-		
-		Thread.sleep(100);
-		try {
-			// Doesn't work
-			System.out.println(base62.decodeBase62("_j+j%"));
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		}
-		
-		
 	}
 }
